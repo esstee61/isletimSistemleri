7e0e453e501c37f23f4d2a7aec07a1f0076b2ede
esstee61 <serkanaraba@gmail.com>
2024-10-24 22:38:37 +0300

    added re

--- Main.java
+++ Main.java
@@ -11,14 +11,17 @@ class Main
                                "iceTEaMachineSAuTomat", "tes1", "tes", "tes1t", 
                                "t4st", "t432", "te", "TESqwer", "teeeeeeest", 
                                "enteresant", "kestane", "Latest", "ttteeesssttt", 
-                               "TaESfdTjklTESTghj", "tesaduf", "tetetetetettest"};
+                               "TaESfdTjklTESTghj", "tesaduf", "tetetetetettest", 
+                               "t", "e", "x", "tets", "etst", "teest", "teat", 
+                               "zxcvbnmyuiophjklqwsdTETghjkl_6TEST", "TESkereTESkereTESkere",
+                               "teat", "1est", ""};
 
         // Measure time for JaroWinkler
         long startJaroWinkler = System.nanoTime();
         for (String str : file_names) {
             int similarity = (int) (JaroWinkler.jaroWinkler(find, str.toLowerCase(), 0.2) * 100);
-            System.out.print(str);
-            System.out.print(" => ");
+            // System.out.print(str);
+            // System.out.print(" => ");
             System.out.println(similarity);
         }
         long endJaroWinkler = System.nanoTime();
@@ -29,12 +32,23 @@ class Main
         long startIsSubsequence = System.nanoTime();
         for (String str : file_names) {
             int result = Solution.isSubsequence(find, str.toLowerCase());
-            System.out.print(str);
-            System.out.print(" => ");
+            // System.out.print(str);
+            // System.out.print(" => ");
             System.out.println(result);
         }
         long endIsSubsequence = System.nanoTime();
         long durationIsSubsequence = endIsSubsequence - startIsSubsequence;
         System.out.println("Time taken by Subsequence: " + durationIsSubsequence + " ns");
+
+        long startRegex = System.nanoTime();
+        for (String str : file_names) {
+            int regex = Re.search(find, str);
+            // System.out.print(str);
+            // System.out.print(" => ");
+            System.out.println(regex);
+        }
+        long endRegex = System.nanoTime();
+        long durationRegex = endRegex - startRegex;
+        System.out.println("Time taken by Subsequence: " + durationRegex + " ns");
     }
 }
--- Solution.java
+++ Solution.java
@@ -2,6 +2,7 @@ class Solution {
     public static int isSubsequence(String s, String t) {
         int sn = s.length();
         int tn = t.length();
+        if (sn == 0 || tn == 0) return -1;
         int i = 0;
         int j = 0;
         while (s.charAt(i) != t.charAt(j)) {
@@ -22,11 +23,12 @@ class Solution {
         if (sn > i*2 || tn*2 < sn) return 0;  // en az ilk yarisi eslesmediyse 0 dondur
 
         int res = 0;
-        if (j - start + 1 > sn * 2) res = isSubsequence(s, t.substring(start+1, tn));  // "tetetetest" vb durumlar icin
+        if (j - start + 1 > sn * 2 || tn-j > sn)  // "tetetetest" ve "TaESfdTjklTESTgh" vb durumlar icin
+            res = isSubsequence(s, t.substring(start+1, tn));
 
         if (sn == (j-start+1) && i == sn) return max(res, (int) (80 + (double) sn / tn * 20));  // full and partial match
 
-        return max(res, (int) (((double) i/sn + (double) ((sn<tn) ? sn: tn)/(j-start+1))*40));  // half match
+        return max(res, (int) (((double) i/sn + (double) ((sn<tn) ? sn: tn)/(j-start+1) + ((j-start+1)/tn))*25));  // half match
     }
 
     public static int max(int a, int b) {
